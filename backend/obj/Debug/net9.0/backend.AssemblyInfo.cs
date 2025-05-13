6d7025c3b0397029b045a555ed5b7b4774bc1f93
JayB111 <jay.ljungby@gmail.com>
2025-05-13 11:15:11 +0200

    Connect backend to Railway MySQL

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e90971dc44e63ca2389fc67e7f9d6296df1aad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c59a8bfc9459806d974aba1e611cd8de8f3c26")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
